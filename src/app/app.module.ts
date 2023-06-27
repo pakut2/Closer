92ea2d9b814040321c1671400509bfc77ae1ee7a
Paweł Kutny <kutny.pawel@gmail.com>
2023-06-28 00:38:34 +0200

    feat(shared): header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
