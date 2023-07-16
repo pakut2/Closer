3bb6de33ddb737e3bf41416f53413f59a25c8595
Paweł Kutny <kutny.pawel@gmail.com>
2023-07-16 14:50:19 +0200

    chore(): capacitor

---
+++ android/app/src/main/java/pakut2/loseyourip/com/MainActivity.java
@@ -0,0 +1,5 @@
+package pakut2.loseyourip.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+export default {
+  appId: "pakut2.loseyourip.com",
+  appName: "closer",
+  webDir: "dist/closer",
+  server: {
+    url: process.env["ANGULAR_APP_LOCAL_URL"],
+    cleartext: true
+  }
+} satisfies CapacitorConfig;
